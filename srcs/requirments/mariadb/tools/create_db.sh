1de4f7d3ad219d6ac4191dcff0699047215ded4c
elhayyany <elhayyanyabdelaziz@gmail.com>
2023-07-12 13:12:44 +0100

    fixing mariadb

--- srcs/requirments/mariadb/tools/create_db.sh
+++ srcs/requirments/mariadb/tools/create_db.sh
@@ -22,14 +22,11 @@ Y
 Y
 _EOF_
 
-#Add a root user on 127.0.0.1 to allow remote connexion
 
 	echo "GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'; FLUSH PRIVILEGES;" | mysql -uroot
 
-#Create database and user for wordpress
 	echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE; GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'; FLUSH PRIVILEGES;" | mysql -uroot
 
-#Import database
 mysql -uroot -p$MYSQL_ROOT_PASSWORD $MYSQL_DATABASE < /usr/local/bin/wordpress.sql
 
 fi
