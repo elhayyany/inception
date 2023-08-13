57efa9b1eb6f1a153fec0751506534bd7d37463d
elhayyany <elhayyanyabdelaziz@gmail.com>
2023-08-13 23:28:46 +0100

    fixing badgetway

--- srcs/requirements/wordpress/tools/create.sh
+++ srcs/requirements/wordpress/tools/create.sh
@@ -1,20 +1,14 @@
 #!/bin/sh
 
-mkdir -p /var/www/wordpress
-cd /var/www/wordpress
-rm -rf *
+mkdir -p /var/www/html/wordpress
+cd /var/www/html/wordpress
+
 wp core download --allow-root
-cp wp-config-sample.php wp-config.php
 
-sed -i 's/listen = \/run\/php\/php-fpm.sock/listen = 9000/g' /etc/php/7.3/fpm/pool.d/www.conf
+wp config create --dbname=$DB_NAME --dbuser=$USER_NAME --dbpass=$DB_PASS --dbhost=mariadb --allow-root
 
-sed -i "s/database_name_here/$DB_NAME/g" wp-config.php
-sed -i "s/username_here/$USER_NAME/g" wp-config.php
-sed -i "s/password_here/$DB_PASS/g" wp-config.php
-sed -i "s/localhost/$DB_HOST/g" wp-config.php
-
-wp core install --url=ael-hayy.42.fr --title=YourBlogTitle --admin_name=aziz --admin_password=havenskey --admin_email=aziz1482002@gmail.com --allow-root
-wp user create user aziz1482002@gmail.com --role=author --user_pass=havenskey --allow-root
+wp core install  --title=YourBlogTitle --admin_name=$WP_ADMIN --admin_password=$WP_ADMIN_PSW --admin_email=$WP_ADMIN_EMAIL --allow-root
+# wp user create user aziz1482002@gmail.com --role=author --user_pass=havenskey --allow-root
 
 
 /usr/sbin/php-fpm7.3 -F
