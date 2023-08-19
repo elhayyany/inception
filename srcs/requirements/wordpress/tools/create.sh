e7eb22313e755ce16f095828d0077898db0fee06
elhayyany <elhayyanyabdelaziz@gmail.com>
2023-08-19 18:32:38 +0100

    finished

--- srcs/requirements/wordpress/tools/create.sh
+++ srcs/requirements/wordpress/tools/create.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+
+sleep 10
 mkdir -p /var/www/html/wordpress
 cd /var/www/html/wordpress
 
@@ -13,7 +15,8 @@ wp config create --dbname=$DB_NAME --dbuser=$USER_NAME --dbpass=$DB_PASS --dbhos
 # wp core install --url=localhost --title=YourBlogTitle --admin_name=$WP_ADMIN --admin_password=$WP_ADMIN_PSW --skip-email --allow-root
 wp core install --url=localhost --title=INXEPTION --admin_name=$WP_ADMIN --admin_password=$WP_ADMIN_PSW --admin_email=$WP_ADMIN_EMAIL --allow-root
 
-wp user create $USER_NAME --role=author --user_pass=$USER_PASS --allow-root --skip-email
+# wp user create $USER_NAME --role=author --user_pass=$DB_PASS --allow-root --skip-email
+wp user create $USER_NAME $USER_EMAIL --role=author --user_pass=$USER_PASS --allow-root 
 
 
 mkdir -p /run/php/
